8046e7972211f7b0e6e7be949315bf2521ca37b8
taewona <nohteawon@gmail.com>
2022-11-23 18:10:43 +0900

    post 임시 완성

--- toyproject/src/main/java/com/laonworks/toyboard/controller/PostController.java
+++ toyproject/src/main/java/com/laonworks/toyboard/controller/PostController.java
@@ -8,116 +8,131 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.laonworks.toyboard.model.Member;
 import com.laonworks.toyboard.model.PostBean;
+import com.laonworks.toyboard.service.MainService;
 import com.laonworks.toyboard.service.PostService;
 
 @Controller
 @Qualifier("post")
 public class PostController {
 
+
 	@Autowired
 	@Qualifier("post")
 	private PostService ps;
 	@Autowired
+	@Qualifier("main")
 	private MainService ms;
-	
+
 	// 글 리스트 전체 불러오기
 	@RequestMapping("/post")
 	public String callPost() {
 		return "post/postmain";
 	}
-	
+
 	// 글 상세보기
 	
-	// 글 작성
+	// 글 작성 폼
 	@RequestMapping("/postwrite")
-	public String postWrite(String post_name, String post_content, HttpSession session) throws Exception {
+	public String postWrite(HttpSession session) {
+		return "postwrite";
+	}
+
+	// 글 작성
+	@RequestMapping("/postinsert")
+	public String postInsert(String post_name, String post_content, HttpSession session) throws Exception {
 		PostBean post = new PostBean();
-		
+
 		// 세션 호출
-		String client_email = session.getAttribute("member_email");
-		
+		String client_email = (String)session.getAttribute("member_email");
+
 		// 이메일로 정보 호출
-		MemberBean client_info = ms. (client_email);
+		Member client_info = ms.getMember(client_email);
 		System.out.println(client_info);
-		
+
 		post.setMember_email(client_email);
-		post.setMember_name(client_info.getMember_name);
+		post.setMember_name(client_info.getMember_name());
 		post.setPost_name(post_name);
 		post.setPost_content(post_content);
-		
+
 		ps.insertPost(post);
-		
-		return "";
+
+		return "post_list.do";
 	}
-	
+
 	// 글 수정 폼
 	@RequestMapping("/postedit")
-	public String postEdit(int post_num,Model model, HttpSession session)  throws Exception {
-		
+	public String postEdit(int post_num, Model model, HttpSession session) throws Exception {
+
 		PostBean post = ps.callOnePost(post_num);
+		
+		// <br>로 변환
+		String post_content = post.getPost_content().replace("\n", "<br>");
+		post.setPost_content(post_content);
+		
 		String returnpage = "";
-		
+
 		// 세션 호출
-		String client_email = session.getAttribute("member_email");
-		
+		String client_email = (String)session.getAttribute("member_email");
+
 		if (client_email.equals(post.getMember_email())) {
-			
+
 			model.addAttribute("post", post);
-			
+
 			returnpage = "";
-		}else {
+		} else {
 			returnpage = "";
 		}
-		
+
 		return returnpage;
 	}
-	
+
 	// 글 수정
 	@RequestMapping("/postrevise")
-	public String postRevise(int post_num, String post_name, String post_content, HttpSession session)  throws Exception {
-		
+	public String postRevise(int post_num, String post_name, String post_content, HttpSession session)
+			throws Exception {
+
 		PostBean post = ps.callOnePost(post_num);
 		String returnpage = "";
-		
+
 		// 세션 호출
-		String client_email = session.getAttribute("member_email");
-		
+		String client_email = (String)session.getAttribute("member_email");
+
 		if (client_email.equals(post.getMember_email())) {
-			
+
 			post.setPost_name(post_name);
 			post.setPost_content(post_content);
-			
+
 			ps.updatePost(post);
 
-			returnpage ="";
-		}else {
-			returnpage ="";
+			returnpage = "";
+		} else {
+			returnpage = "";
 		}
-		
+
 		return returnpage;
 	}
-	
+
 	// 글 삭제
 	@RequestMapping("/postdelete")
 	public String postDelete(int post_num, HttpSession session) throws Exception {
-		
+		session.setAttribute("member_email", "a@test.com");	// 테스트용 세션
 		PostBean post = ps.callOnePost(post_num);
 		String returnpage = "";
-		
+
 		// 세션 호출
-		String client_email = session.getAttribute("member_email");
-		
+		String client_email = (String)session.getAttribute("member_email");
+
 		if (client_email.equals(post.getMember_email())) {
 			ps.deletePost(post_num);
 
 			returnpage = "";
-		}else {
+		} else {
 			returnpage = "";
 		}
-		
+
 		return returnpage;
 	}
-	
-	
+
 }
--- toyproject/src/main/webapp/WEB-INF/views/postwrite.jsp
+++ toyproject/src/main/webapp/WEB-INF/views/postwrite.jsp
@@ -1,13 +1,62 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ include file = "/header.jsp" %>
-    
-<!DOCTYPE html>
-<html>
+	pageEncoding="UTF-8"%>
+<%@ include file = "./header.jsp" %>
+
+<!doctype html>
+<html lang="ko">
 <head>
 <meta charset="UTF-8">
-<title>글 작성</title>
-<body>
+<script
+	src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+	crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+	href="https://fonts.googleapis.com/css2?family=Hahmlet:wght@100;200;400&display=swap"
+	rel="stylesheet">
+<title>글 수정</title>
+<style>
+* {
+	font-family: 'Hahmlet', serif;
+}
+</style>
+</head>
 
+<body>
+	<div class="my-5">
+		<main class="mb-4">
+			<div class="container px-4 px-lg-5">
+				<div class="row gx-4 gx-lg-5 justify-content-center">
+					<form action="/postinsert" method=post>
+					<input type="hidden" name="page" value="${page}">
+					<div class="container List-container">
+						<div class="row mt-1 header">
+							<div class="col-8">
+							<h5 class="content-title">글작성</h5>
+							<h5 class="content-title">제목</h5>
+							<input type="text" name="post_name" value="" required></div>
+							<h5 class="col-1"></h5>
+							<p class="col-8"></p>
+							<p class="col-2"></p>
+						</div>
+						<div class="post-container">
+							<h5 class="content-title">내용</h5>
+							<div class="content">
+							<textarea class="form-control" name="post_content" rows="3" 
+								style="width:90%; height:250px; resize:none;" maxlength="300" placeholder = "내용을 작성해주세요" required></textarea>
+							</div>
+						</div>
+						<div class="board-footer">
+							<button type="submit" class="btn btn-warning update-btn">작성</button>
+							<button type="button" class="btn btn-primary list-btn"
+								onclick="history.go(-1);">취소</button>
+						</div>
+					</div>
+					</form>
+				</div>
+			</div>
+		</main>
+	</div>
 </body>
 </html>
